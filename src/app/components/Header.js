f099e4da7d09ad590092fc44c9771602478b7940
Manish Tomar <Manish.Tomar@uk-cpi.com>
2024-09-18 13:30:27 +0100

    working app with version-1

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,13 @@
 export default function Header() {
     return (
-      <h1 className="text-3xl font-bold text-gray-900 justify-normal">
-        Real-Time Dashboard for Temperature Sensor
-      </h1>
+      <div>
+        <h1 className="text-3xl font-bold text-gray-900 justify-normal">
+          Real-Time Dashboard for Temperature Sensor
+        </h1>
+        <p className="text-lg text-gray-600">
+          Using an ESP32-S3 board with a TMP117 sensor to send real-time temperature data to Firebase and display it on a web dashboard.
+        </p>
+      </div>
     );
   }
   
